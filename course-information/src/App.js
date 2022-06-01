f68ef2d8ff335f4c783226b2903447a266a36dde
Jake Burton <jburto94@yahoo.com>
2022-06-01 10:46:14 -0400

    Separated Content into Parts

--- course-information/src/App.js
+++ course-information/src/App.js
@@ -1,11 +1,13 @@
 const Header = ({ course }) => <h1>{course}</h1>;
 
+const Part = ({ part, exercise }) => <p>{part} {exercise}</p>
+
 const Content = ({ parts, exercises }) => {
   return (
     <div>
-      <p>{parts[0]} {exercises[0]}</p>
-      <p>{parts[1]} {exercises[1]}</p>
-      <p>{parts[2]} {exercises[2]}</p>
+      <Part part={parts[0]} exercise={exercises[0]} />
+      <Part part={parts[1]} exercise={exercises[1]} />
+      <Part part={parts[2]} exercise={exercises[2]} />
     </div>
   )
 };
